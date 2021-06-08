2397d5bf141c8fd8088a4038e67d39b763ffc521
jaybaldha <jbaldha8@gmail.com>
2021-06-08 16:58:32 -0400

    changes done with final desing to update

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import Login from "./components/login.component";
-import SignUp from "./components/signup.component";
+import Login from "./components/Login.js";
+import SignUp from "./components/Signup.js";
 
 function App() {
   return (<Router>
